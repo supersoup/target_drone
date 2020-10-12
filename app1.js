54ac62ffbe1f2b49e4f460068fc4435085d938b8
tangchao <819127251@qq.com>
2020-10-12 18:01:58 +0800

    反射型 xss 攻击

---
+++ app1.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const express = require('express');
+const ejs = require('ejs');
+
+const STATIC_PATH = path.resolve(__dirname, '');
+const app = express();
+const cache = {};
+
+app.set('views', path.join(__dirname, 'views1'))
+app.set('view engine', 'ejs');
+
+app.use(express.static(STATIC_PATH));
+
+app.use('/views1/xssFromBackendTag', function(req, res) {
+	const content = req.query.content;
+	
+	res.set('X-XSS-Protection', '1');
+	
+	res.render('xssFromBackendTag.ejs', {
+		content: content
+	})
+});
+
+app.use('/views1/xssFromBackendAttr', function(req, res) {
+	const content = req.query.content;
+	
+	res.set('X-XSS-Protection', '1');
+	
+	res.render('xssFromBackendAttr.ejs', {
+		content: content
+	})
+});
+
+app.use('/views1/xssFromBackendScript', function(req, res) {
+	const content = req.query.content;
+	
+	res.render('xssFromBackendScript.ejs', {
+		content: content
+	})
+});
+
+app.listen(9000, function () {
+	console.log('第一个服务器：localhost:9000');
+});
---
+++ app2.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const ejs = require('ejs');
+
+app.listen(9001, function () {
+	console.log('第二个服务器：localhost:9000');
+})
